a307c8bfeaa89dd239af9e797cc3717a64ec8e0c
Mr_Rinku_kumar_saini <90628218+MrRinkukumar@users.noreply.github.com>
2022-05-31 23:21:42 +0530

    Create Navbar.js

---
+++ src/Componant/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-primary" >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+          Coding News
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+              <a className="nav-link" href="/">
+                About
+              </a>
+              {/* <a className="nav-link" href="/">
+                About
+              </a> */}
+              {/* <a className="nav-link disabled">Disabled</a> */}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
